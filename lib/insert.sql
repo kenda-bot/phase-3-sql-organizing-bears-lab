c29d653e50f547b3e5f7f1b7ecfb9e23b4c3f740
Margaret Kenda <margaret.kenda@yahoo.com>
2022-10-29 16:33:29 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate',5,'M','black','sanguine','true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy',3,'M','black','apathetic','true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha',6,'F','black','choleric','false');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown',9,'F','grey','choleric','true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa',4,'M','brown','choleric','true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch',9,'F','grey','choleric','true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy',8,'F','black','choleric','true');
+INSERT INTO bears (name) VALUES (NULL);
+
+
+
+
